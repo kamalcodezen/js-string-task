9a91a92948404fcd86b52e0204f22e55ecaee7af
kamalcodezen <kamaluddin7908@gmail.com>
2026-02-08 04:55:12 +0530

    task 3 complpete

---
+++ javaScript/Task-3.js
@@ -0,0 +1,8 @@
+// Check whether a string contains all the vowels a, e, i, o, u
+let name = "kamlauddin";
+
+if (name.toLowerCase().includes("a") && name.toLowerCase().includes("e") && name.toLowerCase().includes("i") && name.toLowerCase().includes("o") && name.toLowerCase().includes("u")) {
+    console.log("true");
+} else {
+    console.log("false");
+}
